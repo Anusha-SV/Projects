4a36e7da6eda774abfb3f0fc9c4d25cf7ea1699a
Anusha-SV <anushasv2.xworks@gmail.com>
2024-08-26 09:48:43 +0530

    07/08

--- Xworkz/Market.java
+++ Xworkz/Market.java
@@ -1,61 +1,20 @@
-class Market{
-	static int hpPrice=50000;
-	static int dellP=60000;
-	static int lenovoP=70000;
-	static int asusP=65000;
-	static int keyboardP=500;
-	static int mouseP=250;
-	public static int sale(String laptop,boolean offers){
-		int totalPrice=0;
-		if(laptop=="HP" ){
-				if(offers==true ){
-			totalPrice=hpPrice+mouseP;
-			System.out.println("For HP laptop the offer is mouse="+totalPrice);
-			return totalPrice;
-			}
-		else{
-			System.out.println("NO OFFERS");
-			totalPrice=hpPrice;
-			return totalPrice;
-		}
+package com.xworkz.Market.store;
+
+public class Market {
+
+	private String name;
+	private String location;
+	
+	public Market(String name, String location) {
+
+		this.name = name;
+		this.location = location;
 	}
-		if(laptop=="Dell" ){
-				if(offers==true ){
-			totalPrice=dellP+keyboardP;
-			System.out.println("For DEll laptop the offer is Keyboard="+totalPrice);
-			return totalPrice;
-		}
-		else{
-			System.out.println("NO OFFERS");
-			totalPrice=dellP;
-			return totalPrice;
-		}
+	
+	public void show() {
+		System.out.println("The name of market is : "+name);
+		System.out.println("The name of location is : "+location);
+		System.out.println("========================================");
 	}
-		if(laptop=="lenovo"){
-			if(offers==true ){
-			totalPrice=lenovoP+mouseP;
-			System.out.println("For LENOVO laptop the offer is mouse="+totalPrice);
-			return totalPrice;
-		}
-		else{
-			System.out.println("NO OFFERS");
-			totalPrice=lenovoP;
-			return totalPrice;
-		}
-	}
-		if(laptop=="asus"){
-			if(offers==false ){
-			totalPrice=asusP;
-			System.out.println("For LENOVO laptop no offers="+totalPrice);
-			return totalPrice;
-		}
-		else{
-			System.out.println("NO OFFERS");
-			totalPrice=asusP;
-		}
-	}
-	return totalPrice;
-	}
+	
 }
-	
-	
--- Xworkz/MarketRunner.java
+++ Xworkz/MarketRunner.java
@@ -1,5 +1,23 @@
-class MarketRunner{
-public static void main(String []args){
-	Market.sale("HP",true);
-}
+package com.xworkz.Market;
+
+import com.xworkz.Market.store.Market;
+import com.xworkz.Market.store.show.Data;
+
+public class MarketRunner {
+
+	public static void main(String[] args) {
+		
+		Market m = new Market("Reliance", "Rajajinagara");
+		Market m1 = new Market("Zudio", "Mejastic");
+		Market m2 = new Market("More", "Yashwanthpura");
+		
+		Data d = new Data();
+		
+		d.store(m);
+		d.store(m1);
+		d.store(m2);
+		d.display();
+		
+	}
+
 }
